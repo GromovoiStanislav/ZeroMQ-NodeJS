13e12166acce44fa710842867963cf20eda935e1
Gromovoi Stanislav <gromila12@rambler.ru>
2023-09-18 11:05:02 +0600

    Project 1

--- Project 1/producer.js
+++ Project 1/producer.js
@@ -8,3 +8,17 @@ setInterval(function () {
   console.log('sending work');
   sock.send('some work');
 }, 500);
+
+// sock.bind('tcp://127.0.0.1:3000', (error) => {
+//   if (error) {
+//     console.log(error);
+//     process.exit(0);
+//   }
+
+//   console.log('Producer bound to port 3000');
+
+//   setInterval(function () {
+//     console.log('sending work');
+//     sock.send('some work');
+//   }, 500);
+// });
--- Project 1/publisher.js
+++ Project 1/publisher.js
@@ -8,3 +8,17 @@ setInterval(function () {
   console.log('sending a multipart message envelope');
   sock.send(['kitty cats', 'meow!']);
 }, 500);
+
+// sock.bind('tcp://127.0.0.1:3000', (error) => {
+//   if (error) {
+//     console.log(error);
+//     process.exit(0);
+//   }
+
+//   console.log('Publisher bound to port 3000');
+
+//   setInterval(function () {
+//     console.log('sending a multipart message envelope');
+//     sock.send(['kitty cats', 'meow!']);
+//   }, 500);
+// });
